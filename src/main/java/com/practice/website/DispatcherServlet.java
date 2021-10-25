904931c1a141d4055c46acc86ebb1d54ffdb063f
jungwoo <jungwoo5759@gmail.com>
2021-10-25 16:26:28 +0900

    feat: 프로젝트 초기 구성 및 dispatcherServlet 추가

---
+++ src/main/java/com/practice/website/DispatcherServlet.java
@@ -0,0 +1,22 @@
+package com.practice.website;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "dispatcherServlet", value = {"/"})
+public class DispatcherServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+}
