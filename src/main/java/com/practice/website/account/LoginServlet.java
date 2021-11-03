00559c4eef2fb11261712265d62d59c980ac3d3a
jungwoo <jungwoo5759@gmail.com>
2021-11-03 10:27:57 +0900

    refactor: 폴더 구조 변경

---
+++ src/main/java/com/practice/website/account/LoginServlet.java
@@ -0,0 +1,21 @@
+package com.practice.website.account;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        System.out.println(email + " : " + password);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
