e4715f0091fc5627a33110cf48bc84c07555db6b
jungwoo <jungwoo5759@gmail.com>
2021-11-07 17:29:34 +0900

    feat: collection 예외 추가

---
+++ src/main/java/com/practice/website/collection/exception/NotFoundCollectionException.java
@@ -0,0 +1,10 @@
+package com.practice.website.collection.exception;
+
+public class NotFoundCollectionException extends Exception {
+    public NotFoundCollectionException() {
+    }
+
+    public NotFoundCollectionException(String message) {
+        super(message);
+    }
+}
